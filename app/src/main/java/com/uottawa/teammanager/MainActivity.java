34b0e1b3b3317831578b34a843886d558ebc4f50
Maxime <mcote074@uottawa.ca>
2017-10-04 19:09:06 -0400

    add repo
    add repo

---
+++ app/src/main/java/com/uottawa/teammanager/MainActivity.java
@@ -0,0 +1,80 @@
+package com.uottawa.teammanager;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnOpenInGoogleMaps (View view) {
+        EditText teamAddres = (EditText) findViewById(R.id.teamAddressField);
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+teamAddres.getText());
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+    public void OnSetAvatarButton(View view) {
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivityForResult (intent,0);
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_CANCELED) return;
+        ImageView avatarImage = (ImageView) findViewById(R.id.avatarImage);
+        String drawableName; //4
+        switch (data.getIntExtra("imageID",R.id.teamid00)) {//Figuring out the correct image
+            case R.id.teamid00:
+                drawableName = "ic_logo_00";
+                break;
+            case R.id.teamid01:
+                drawableName = "ic_logo_01";
+                break;
+            case R.id.teamid02:
+                drawableName = "ic_logo_02";
+                break;
+            case R.id.teamid03:
+                drawableName = "ic_logo_03";
+                break;
+            case R.id.teamid04:
+                drawableName = "ic_logo_04";
+                break;
+            case R.id.teamid05:
+                drawableName = "ic_logo_05";
+                break;
+            default:
+                drawableName = "ic_logo_00";
+                break;
+        } //27
+        int resID = getResources().getIdentifier(drawableName, "drawable",  getPackageName());
+        avatarImage.setImageResource(resID);
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/uottawa/teammanager/ProfileActivity.java
@@ -0,0 +1,54 @@
+package com.uottawa.teammanager;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.Date;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void setTeamIcon(View view){
+        Intent returnIntent = new Intent();
+        ImageView selectedImage = (ImageView) view;
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
+
+    public void notImplement(View view) {
+        Snackbar.make(view, "Not Implemented", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+
+}
